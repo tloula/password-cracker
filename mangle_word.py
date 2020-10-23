e04d4079bbb8f21fdee08dcfb29684bedc4e4c58
Trevor Loula <trevorloula@outlook.com>
2020-10-23 19:49:13 -0400

    Cracked Another One

--- mangle_word.py
+++ mangle_word.py
@@ -1,11 +1,14 @@
 def mangle_word(word):
     output = []
-    #for i in range(0, 1000):
-    #    output.append(str(i) + word)
-    word = word.replace("e", "3")
-    word = word.replace("i", "!")
-    word = word.replace("s", "$")
-    word = word.replace("a", "@")
-    word = word.replace("o", "0")
-    word = word.replace("g", "9")
-    return [word]
+    #word = word.replace("e", "3")
+    #word = word.replace("i", "1")
+    #word = word.replace("s", "$")
+    #word = word.replace("a", "@")
+    #word = word.replace("o", "0")
+    #word = word.replace("g", "9")
+    #for i in range(0, 100):
+    #    output.append(word + str(i))
+    for i in range(0, len(word)):
+        newword = word[:i] + word[i].upper() + word[i + 1:]
+        output.append(newword)
+    return output
